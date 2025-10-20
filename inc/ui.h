200398210bf32331b2da19c51c9d032c6231c37b
evtish <evevtish@gmail.com>
2025-10-20 18:47:56 +0300

    Add UI functions

---
+++ inc/ui.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void input_val(string &str, const string &message);
+void output_val(const string &val, const string &message);
+void output_val(const double &val, const string &message);
+void output_stations(const vector<const string*> &reversed_stations);
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,21 @@
 #include <vector>
 
 #include "station.h"
+#include "ui.h"
 
 using namespace std;
 
 int main(void) {
     setlocale(LC_ALL, "");
 
+    string passenger_name;
+    input_val(passenger_name, "ФИО");
+
+    string passenger_id_card;
+    input_val(passenger_id_card, "Номер и серия паспорта (слитно)");
+
     string dest_station_name;
-    cout << "Куда: ";
-    getline(cin, dest_station_name);
+    input_val(dest_station_name, "Куда");
 
     if (!stations.count(dest_station_name)) {
         fprintf(stderr, "Ошибка: станция не найдена\n");
@@ -18,24 +24,23 @@ int main(void) {
     }
 
     const double total_dist = stations.at(dest_station_name).dist_from_departure,
-        total_price = total_dist * price_per_km;
+        total_cost = total_dist * price_per_km;
 
-    vector<const string*> route_station_names {&dest_station_name};
+    vector<const string*> route_stations {&dest_station_name};
     for (
-        const string* prev_station_name = stations.at(*route_station_names.back()).prev_station_name;
+        const string* prev_station_name = stations.at(*route_stations.back()).prev_station_name;
         prev_station_name != nullptr;
-        prev_station_name = stations.at(*route_station_names.back()).prev_station_name
+        prev_station_name = stations.at(*route_stations.back()).prev_station_name
     ) {
-        route_station_names.push_back(prev_station_name);
+        route_stations.push_back(prev_station_name);
     }
 
-    cout << "Расстояние: " << total_dist << " км\nСтоимость: " << total_price << " р.\nСтанции: ";
-    for (size_t i = route_station_names.size(); i > 0; i--) {
-        cout << *route_station_names[i - 1];
-        if (i > 1)
-            cout << ", ";
-    }
-    cout << "\n";
+    output_val(total_dist, "Расстояние, км");
+    output_val(total_cost, "Стоимость, руб");
+    output_val(passenger_name, "ФИО");
+    output_val(passenger_id_card, "Номер и серия паспорта");
+
+    output_stations(route_stations);
 
     return 0;
 }
